16dbe7533d177437f134404d2cf6d8e0050e31ae
sukanturkyilmaz <139549371+sukanturkyilmaz@users.noreply.github.com>
2025-10-20 00:43:37 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,8 +16,4 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  define: {
-    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify('https://ebmjfuvairqzxsisshep.supabase.co'),
-    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVibWpmdXZhaXJxenhzaXNzaGVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkzMDk5ODUsImV4cCI6MjA3NDg4NTk4NX0.Vm0cgAW0Oxt-DXKbs1TNIYbA-Zqp7b2rkI9arFEhZic'),
-  },
 });
